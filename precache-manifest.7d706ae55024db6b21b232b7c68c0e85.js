1372fbf7751d190de909d651337b4ea7d5565463
Habiba <heid3424@gmail.com>
2024-06-03 14:20:58 +0300

    Updates

---
+++ precache-manifest.7d706ae55024db6b21b232b7c68c0e85.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6dd2fba088bd331bc2494121bf1ece",
+    "url": "/Register-page/index.html"
+  },
+  {
+    "revision": "c26c505a2768fcf78142",
+    "url": "/Register-page/static/css/main.c2a7ca5d.chunk.css"
+  },
+  {
+    "revision": "e0b9e1d04f95285da8d4",
+    "url": "/Register-page/static/js/2.090dd0f4.chunk.js"
+  },
+  {
+    "revision": "8f13608f3089cd6924ef",
+    "url": "/Register-page/static/js/3.67d59c16.chunk.js"
+  },
+  {
+    "revision": "c26c505a2768fcf78142",
+    "url": "/Register-page/static/js/main.bda6fb7d.chunk.js"
+  },
+  {
+    "revision": "fbf9ca7c57174ca23513",
+    "url": "/Register-page/static/js/runtime~main.1d69a7bc.js"
+  },
+  {
+    "revision": "1931427c23716bb4ff98d03627f7137c",
+    "url": "/Register-page/static/media/fa-brands-400.1931427c.ttf"
+  },
+  {
+    "revision": "b6356c957274676e6571c1ff5e11c9a8",
+    "url": "/Register-page/static/media/fa-brands-400.b6356c95.woff2"
+  },
+  {
+    "revision": "023a4a925fa3fce0f66b769ef6bbb264",
+    "url": "/Register-page/static/media/fa-regular-400.023a4a92.woff2"
+  },
+  {
+    "revision": "a0cc1c8265e3163aa654a5284ea11ace",
+    "url": "/Register-page/static/media/fa-regular-400.a0cc1c82.ttf"
+  },
+  {
+    "revision": "25914cfeafced317e1a0372187fbb2b9",
+    "url": "/Register-page/static/media/fa-solid-900.25914cfe.ttf"
+  },
+  {
+    "revision": "6c4eee562650e53cee32496bdfbe534b",
+    "url": "/Register-page/static/media/fa-solid-900.6c4eee56.woff2"
+  },
+  {
+    "revision": "71808e147dc6d82c198a4ba292c0cb69",
+    "url": "/Register-page/static/media/fa-v4compatibility.71808e14.ttf"
+  },
+  {
+    "revision": "c9e50ccabe9e36f370272197595ea1e5",
+    "url": "/Register-page/static/media/fa-v4compatibility.c9e50cca.woff2"
+  }
+]);
